a3eb48f28ad2f123be7c6c5f48c0313aa7e4fd95
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-07-13 11:32:58 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { InlineStyle } from "./components/InlineStyle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
